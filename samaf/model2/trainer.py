a3860b40a80777e2c199210c614a57f59ab99e5b
Pasindu Wijesena <pasindu@the-ai.team>
2021-12-14 10:26:19 +0530

    feat(samaf): triplet loss

--- samaf/model2/trainer.py
+++ samaf/model2/trainer.py
@@ -4,25 +4,116 @@ import torch
 import numpy as np
 import librosa.display
 
+from pytorch_metric_learning import losses, miners, distances, reducers, testers
+from pytorch_metric_learning.utils import accuracy_calculator
+
 from model import Model
 from dataset import PerformanceChunks
+from utils import combine_dimensions
 
 import argparse
 import wandb
 
 
-def train(model, loss_fn, device, dataloader, optimizer, epoch):
+def get_sequence_hash_generator_function():
+    cache = dict()
+    next_id = 1
+
+    def get_sequence_id(work_ids, track_ids, offset_indexes):
+        nonlocal next_id
+        chunk_ids = []
+        for (index, work_id) in enumerate(work_ids):
+            hash_key = "%s:%s:%d" % (
+                work_id, track_ids[index], offset_indexes[index])
+            if hash_key not in cache:
+                cache[hash_key] = next_id
+                next_id += 1
+            chunk_ids.append(cache[hash_key])
+
+        return chunk_ids
+
+    return get_sequence_id
+
+
+def get_loss_function(config):
+
+    alpha = 0.6  # get from config
+
+    threshold_reducer_low = 0  # get from config
+    margin = 0.3  # get from config
+
+    autoencoder_loss = torch.nn.CrossEntropyLoss()
+
+    distance = distances.CosineSimilarity()
+    reducer = reducers.ThresholdReducer(low=threshold_reducer_low)
+    print("distance, reducer initialized")
+
+    triplet_loss = losses.TripletMarginLoss(
+        margin=margin, distance=distance, reducer=reducer)
+
+    def loss_fn(expected_indices_sequence, reconstructed_sequence, embeddings, sequence_ids, triplet_indices):
+        # expected_indices_sequence -> [batch_size * variations, sequence_length]
+        # reconstructed_sequence -> [batch_size * variations, sequence_length, feature_size]
+        # embeddings -> [batch_size * variations, hidden_size]
+        # sequence_ids -> [batch_size * variations]
+
+        feature_size = reconstructed_sequence.shape[-1]
+
+        input = reconstructed_sequence.view(-1, feature_size)
+        target = expected_indices_sequence.view(-1)
+
+        loss = alpha * autoencoder_loss(input, target) + (1-alpha) * \
+            triplet_loss(embeddings, sequence_ids, triplet_indices)
+        return loss
+
+    return loss_fn
+
+
+def get_mining_function(config):
+    margin = 0.3  # get from config
+    type_of_triplets = "semihard"  # get from config
+    distance = distances.CosineSimilarity()
+
+    return miners.TripletMarginMiner(
+        margin=margin, distance=distance, type_of_triplets=type_of_triplets)
+
+
+def train(model, loss_fn, sequence_id_generator_fn, mining_fn, device, dataloader, optimizer, epoch):
     model.train()
     losses = []
 
-    for i, (sequence, sequence_indices, work_id, track_id) in enumerate(dataloader):
-        sequence, sequence_indices = sequence.to(
-            device), sequence_indices.to(device)
+    for i, (sequence, sequence_indices, work_id, track_id, offset_index) in enumerate(dataloader):
+        variations = sequence.shape[1]
+        sequence = combine_dimensions(sequence, 0, 1).to(device)
+        sequence_indices = combine_dimensions(
+            sequence_indices, 0, 1).to(device)
 
+        sequence_ids = torch.from_numpy(  # broadcast the results to all the variations
+            np.array(sequence_id_generator_fn(work_id, track_id, offset_index)).reshape(-1, 1) *
+            # [batch_size] * [number of variations]
+            np.ones(len(offset_index), variations)
+        ).view(-1).to(device)
+
+        # Triplets
+        triplets = mining_fn(embeddings, sequence_ids)
+        anchor, positive, negative = triplets
+
+        max_index = 0
+        if anchor.shape[0] > 0:
+            max_index = np.max([np.max(anchor.numpy()), np.max(
+                positive.numpy()), np.max(negative.numpy())])
+
+        if max_index >= sequence.shape[0]:
+            print(
+                "[PREVENTED ERROR] Found index {} on the mined indices".format(max_index))
+            print("Skipping the training iteration")
+            return
+
+        # Compute loss and optimize
         optimizer.zero_grad()
         (embeddings, reconstructed_sequence) = model(sequence)
         loss = 1.0 * loss_fn(sequence_indices,
-                             reconstructed_sequence, embeddings)
+                             reconstructed_sequence, embeddings, sequence_ids, triplets)
         loss.backward()
         optimizer.step()
 
@@ -47,6 +138,7 @@ def train(model, loss_fn, device, dataloader, optimizer, epoch):
 
 
 def validate(model, loss_fn, device, dataloader, epoch):
+    # TODO: Update the logic here
     losses = []
 
     with torch.no_grad():
@@ -68,23 +160,6 @@ def validate(model, loss_fn, device, dataloader, epoch):
         {"validation_loss": np.mean(losses)}, commit=False)
 
 
-def get_loss_function():
-
-    autoencoder_loss = torch.nn.CrossEntropyLoss()
-
-    def loss_fn(expected_indices_sequence, reconstructed_sequence, embeddings):
-        # expected_indices_sequence -> [batch_size, sequence_length]
-        # reconstructed_sequence -> [batch_size, sequence_length, feature_size]
-        feature_size = reconstructed_sequence.shape[-1]
-
-        input = reconstructed_sequence.view(-1, feature_size)
-        target = expected_indices_sequence.view(-1)
-
-        return autoencoder_loss(input, target)
-
-    return loss_fn
-
-
 def drive(config):
     with wandb.init(project=config.wandb_project_name, name=config.wandb_run_name,
                     job_type="train", entity="pasinducw", config=config) as wandb_run:
@@ -95,7 +170,10 @@ def drive(config):
             time_axis=config.time_axis,
             hop_length=config.hop_length,
             frames_per_sample=config.frames_per_sample,
-            cache_limit=config.dataset_cache_limit
+            cache_limit=config.dataset_cache_limit,
+            include_augmentations=True,
+            augmentations_base_dir=config.augmentations_base_dir,
+            augmentations=config.augmentations,
         )
         train_dataloader = torch.utils.data.DataLoader(
             train_dataset, batch_size=config.batch_size, num_workers=config.workers, shuffle=True)
@@ -107,7 +185,10 @@ def drive(config):
             time_axis=config.time_axis,
             hop_length=config.hop_length,
             frames_per_sample=config.frames_per_sample,
-            cache_limit=config.dataset_cache_limit
+            cache_limit=config.dataset_cache_limit,
+            include_augmentations=True,
+            augmentations_base_dir=config.augmentations_base_dir,
+            augmentations=config.augmentations,
         )
         validation_dataloader = torch.utils.data.DataLoader(
             validation_dataset, batch_size=config.batch_size, num_workers=config.workers, shuffle=False)
@@ -118,20 +199,25 @@ def drive(config):
 
         optimizer = torch.optim.Adagrad(
             model.parameters(), lr=config.learning_rate)
-        loss_fn = get_loss_function()
+        chunk_id_generator_fn = get_sequence_hash_generator_function()
+        
+        loss_fn = get_loss_function(config)
+        mining_fn = get_mining_function(config)
 
         if config.model_snapshot:
             print("Loading model snapshot")
-            model_snapshot = wandb_run.restore("model.pth", run_path=config.model_snapshot)
+            model_snapshot = wandb_run.restore(
+                "model.pth", run_path=config.model_snapshot)
             model.load_state_dict(model_snapshot["model"])
 
         wandb.watch(model, criterion=loss_fn, log="all")
         print("Configurations done. Commence model training")
 
         for epoch in range(1, config.epochs+1):
-            train(model, loss_fn, device, train_dataloader, optimizer, epoch)
-            if config.validate:
-                validate(model, loss_fn, device, validation_dataloader, epoch)
+            train(model, loss_fn, chunk_id_generator_fn, mining_fn,
+                  device, train_dataloader, optimizer, epoch)
+            # if config.validate:
+            #     validate(model, loss_fn, device, validation_dataloader, epoch)
             wandb.log({"epoch": epoch})
 
             model_checkpoint_dir = os.path.join(
